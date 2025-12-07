2cc2ca0df4ded301c84d5e0b081ea63ee63f00ce
Tyrone Scafe <tscafejr@gmail.com>
2025-12-07 11:51:37 -0500

    finish day 2

--- src/days/two/main.rs
+++ src/days/two/main.rs
@@ -1,11 +1,135 @@
 use crate::days::Day;
+use regex::Regex;
+use std::collections::HashSet;
 
-fn part1(_data: Vec<String>) -> u64 {
-    0
+type ValidatorFn = fn(n: u64) -> bool;
+
+struct Range {
+    pub low: u64,
+    pub high: u64,
+    validator: ValidatorFn,
 }
 
-fn part2(_data: Vec<String>) -> u64 {
-    0
+pub fn get_factors(n: u64) -> Vec<u64> {
+    let mut v = Vec::new();
+    for i in 1..n {
+        if n % i == 0 {
+            v.push(i)
+        }
+    }
+    v
+}
+
+pub fn split_into_chunks(s: &str, n: u64) -> Vec<String> {
+    s.chars()
+        .collect::<Vec<char>>()
+        .chunks(n as usize)
+        .map(|chunk| chunk.iter().collect())
+        .collect()
+}
+
+pub fn is_invalid(n: u64) -> bool {
+    let s = n.to_string();
+
+    if s.len() % 2 != 0 {
+        return false;
+    }
+
+    let (l, r) = s.split_at(s.len() / 2);
+
+    return l == r;
+}
+
+pub fn is_invalid_p2(n: u64) -> bool {
+    let s = n.to_string();
+    let f = get_factors(s.len() as u64);
+
+    for i in f {
+        let mut seen: HashSet<String> = HashSet::new();
+        let buckets = split_into_chunks(&s, i);
+        let mut i_valid = false;
+
+        for b in buckets {
+            if seen.is_empty() {
+                seen.insert(b);
+            } else {
+                if !seen.contains(&b) {
+                    i_valid = true;
+                    break;
+                }
+            }
+        }
+
+        if !i_valid {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+impl Range {
+    pub fn new(s: &str, validator_fn: ValidatorFn) -> Self {
+        let re = Regex::new(r"(\d+)-(\d+)").unwrap();
+        let mut h = 0;
+        let mut l = 0;
+
+        for cap in re.captures_iter(&s) {
+            l = cap[1].parse().unwrap();
+            h = cap[2].parse().unwrap();
+        }
+
+        Range {
+            low: l,
+            high: h,
+            validator: validator_fn,
+        }
+    }
+
+    pub fn find_duplicates(&self) -> Vec<u64> {
+        let mut d = Vec::new();
+
+        for n in self.low..self.high + 1 {
+            if (self.validator)(n) {
+                d.push(n)
+            }
+        }
+        d
+    }
+}
+
+fn part1(data: Vec<String>) -> u64 {
+    let mut sum = 0;
+
+    for i in 0..data.len() {
+        let s = data[i].split(",");
+        for entry in s {
+            let r = Range::new(entry, is_invalid);
+            let d = r.find_duplicates();
+            for j in 0..d.len() {
+                sum += d[j]
+            }
+        }
+    }
+
+    sum
+}
+
+fn part2(data: Vec<String>) -> u64 {
+    let mut sum = 0;
+
+    for i in 0..data.len() {
+        let s = data[i].split(",");
+        for entry in s {
+            let r = Range::new(entry, is_invalid_p2);
+            let d = r.find_duplicates();
+            for j in 0..d.len() {
+                sum += d[j]
+            }
+        }
+    }
+
+    sum
 }
 
 pub fn functions() -> Day {
--- src/days/two/test.rs
+++ src/days/two/test.rs
@@ -1,4 +1,10 @@
 #[cfg(test)]
+use super::main::get_factors;
+#[cfg(test)]
+use super::main::is_invalid;
+#[cfg(test)]
+use super::main::is_invalid_p2;
+#[cfg(test)]
 use crate::days;
 #[cfg(test)]
 use crate::util;
@@ -14,14 +20,55 @@ mod tests {
         util::files::read_file_line_by_line(data_path)
     }
 
-    #[ignore]
+    #[test]
+    fn valid_id() {
+        let result = is_invalid(123);
+        assert_eq!(result, false)
+    }
+
+    #[test]
+    fn invalid_id() {
+        let result = is_invalid(123123);
+        assert_eq!(result, true)
+    }
+
+    #[test]
+    fn valid_id_p2() {
+        let result = is_invalid_p2(123);
+        assert_eq!(result, false)
+    }
+
+    #[test]
+    fn invalid_id_p2() {
+        let result = is_invalid_p2(123123);
+        assert_eq!(result, true)
+    }
+
+    #[test]
+    fn invalid_id_p2_special() {
+        let result = is_invalid_p2(111);
+        assert_eq!(result, true)
+    }
+
+    #[test]
+    fn invalid_id_p2_special_repeating() {
+        let result = is_invalid_p2(2121212121);
+        assert_eq!(result, true)
+    }
+
+    #[test]
+    fn get_factors_test() {
+        let factors = get_factors(9);
+        assert_eq!(factors.len(), 2);
+    }
+
     #[test]
     fn p1() {
         if let Some(day) = days::get_day_from_str("two") {
             if let Some(parsed_fns) = days::get_day(day) {
                 if let Some(part1_fn) = parsed_fns.part1 {
                     let result = part1_fn(get_data());
-                    assert_eq!(result, 1);
+                    assert_eq!(result, 1227775554);
                     return;
                 }
 
@@ -34,14 +81,13 @@ mod tests {
         }
     }
 
-    #[ignore]
     #[test]
     fn p2() {
         if let Some(day) = days::get_day_from_str("two") {
             if let Some(parsed_fns) = days::get_day(day) {
                 if let Some(part2_fn) = parsed_fns.part2 {
                     let result = part2_fn(get_data());
-                    assert_eq!(result, 1);
+                    assert_eq!(result, 4174379265);
                     return;
                 }
 
